6febaa99e069ceb627d7281d96fea8c20f1f46c3
Vanessa Schneider <96793906+VanessaSchneider@users.noreply.github.com>
2022-02-16 12:18:12 -0500

    Add files via upload

--- QuestionForm.js
+++ QuestionForm.js
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
 
-      function QuestionForm() {
+      function QuestionForm({handleAddQuestion}) {
         const [question, setQuestion] = useState("");
         const [category, setCategory] = useState("");
-        const [answer, setAnswer] = useState("");
+        const [answers1, setAnswers1] = useState("");
+        const [answers2, setAnswers2] = useState("");
+        const [answers3, setAnswers3] = useState("");
+        const [answers4, setAnswers4] = useState("");
+        const [correctIndex, setCorrectIndex] = useState("");
         
       
       
@@ -15,8 +19,21 @@ import React, { useState } from "react";
           setCategory(event.target.value);
         }
 
-        function handleAnswer(event) {
-          setAnswer(event.target.value);
+        function handleAnswer1(event) {
+          setAnswers(event.target.value);
+        }
+        function handleAnswer2(event) {
+          setAnswers(event.target.value);
+        }
+        function handleAnswer3(event) {
+          setAnswers(event.target.value);
+        }
+        function handleAnswer4(event) {
+          setAnswers(event.target.value);
+        }
+
+        function handleIndex(event) {
+          setCorrectIndex(event.target.value);
         }
 
 
@@ -24,14 +41,49 @@ import React, { useState } from "react";
           e.preventDefault();
             const formData = {
                question: question,
-                category: category };
+                category: category,
+                answers: [answers],
+                correctIndex: parseInt(correctIndex),
+              };
+
+        fetch("http://localhost:8003/questions", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        })
+          
+        handleAddQuestion(formData);
         }
-      
+
         return (
           <form onSubmit={handleSubmit}>
-          <input type="text"  onChange={handleQuestion} value={question} />
-          <input type="text"  onChange={handleAnswer} value={answer} />
-          <input type="text"  onChange={handleCategory} value={category} />
+              <div className="catTile ui card">
+                <div>
+          <select
+      name = "sort"
+      onChange={handleCategory}
+      value = {category}>
+        <option value = "Entertainment">Entertainment</option>
+        <option value = "Art">Art</option>
+        <option value = "Pokemon">Pokemon</option>
+        <option value = "History">History</option>
+        <option value = "Science">Science</option>
+  n      <option value = "Literature">Literature</option>
+        </select>  
+          <input type="text" placeholder="Type your question"onChange={handleQuestion} value={question} />
+          </div>
+          </div>
+          <div className="index">
+            <div>
+          <input type="text" placeholder = "Type your answers" onChange={handleAnswer1} value={answers} />
+          <input type="text" placeholder = "Type your answers" onChange={handleAnswer2} value={answers} />
+          <input type="text" placeholder = "Type your answers" onChange={handleAnswer3} value={answers} />
+          <input type="text" placeholder = "Type your answers" onChange={handleAnswer4} value={answers} />
+          </div>
+          </div>
+          <input type="text" placeholder = "Which answer is correct" onChange={handleIndex} value={correctIndex} />
           <button type="submit">Submit</button>
         </form>
         );
